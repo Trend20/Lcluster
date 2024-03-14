644f21df24da2b7d12c854e1f2036252b3fb2dfd
Trend20 <enockomondi305@gmail.com>
2024-03-14 16:33:14 +0300

    header scroll fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,29 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 
 const Header = () => {
+  const [scrollY, setScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  const isScrolled = scrollY > 0;
   return (
-    <div className="px-50 flex w-full py-5">
+    <div
+      className={`fixed top-0 left-0 w-full px-50 py-3 z-50 ${
+        isScrolled ? "bg-boxdark shadow-md transition-all duration-300" : ""
+      }`}
+    >
       <div className="w-full flex justify-between items-center">
         <div className="flex justify-start w-1/4 items-center">
           <Link className="flex items-center space-x-2" href="/">
@@ -18,6 +36,7 @@ const Header = () => {
             />
           </Link>
         </div>
+        {/* px-50 flex w-full py-5 fixed inset-x-0 top-0 shadow-sm translate-y-[scroll] transition-transform */}
         <div className="md:flex w-1/4 flex justify-center items-center">
           <nav className="flex items-center space-x-2">
             <Link className="text-normal" href="#features">
