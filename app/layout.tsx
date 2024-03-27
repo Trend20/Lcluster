66d6f44500da92f7b330793b28dda7fac3c07600
Trend20 <enockomondi305@gmail.com>
2024-03-27 21:48:21 +0300

    loader screen and hiding footer and header component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
-import type { Metadata } from "next";
+"use client";
+
 import "../styles/globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -6,26 +7,36 @@ import { NextAuthProvider } from "./providers";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
 import { usePathname } from "next/navigation";
-
-export const metadata: Metadata = {
-  title: "CodeCrony",
-  description: "CodeCrony",
-};
+import { useEffect, useState } from "react";
+import Loader from "@/components/common/Loader";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathname = usePathname();
+  const isLogin = pathname === "/auth/signin";
+  const isRegister = pathname === "/auth/signup";
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
+
   return (
     <html lang="en">
-      <body>
+      <body suppressHydrationWarning={true}>
         <NextAuthProvider>
-          <div className="flex w-full flex-col">
-            <Header />
-            <main className="min-h-screen">{children}</main>
-            <Footer />
-          </div>
+          {loading ? (
+            <Loader />
+          ) : (
+            <div className="flex w-full flex-col">
+              {!isLogin && !isRegister && <Header />}
+              <main className="min-h-screen">{children}</main>
+              {!isLogin && !isRegister && <Footer />}
+            </div>
+          )}
         </NextAuthProvider>
         <SpeedInsights />
         <Analytics />
