00e2c47ea533d9a5ec016e22d8000c63a9fdc278
Trend20 <enockomondi305@gmail.com>
2024-03-27 18:55:18 +0300

    hidding footer on login and signup routes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { FaXTwitter } from "react-icons/fa6";
 import { FiLinkedin } from "react-icons/fi";
@@ -44,32 +44,47 @@ const FooterLinks = [
 
 const Footer = () => {
   const pathname = usePathname();
+  const [showHeaderAndFooter, setShowHeaderAndFooter] = useState(false);
+
+  const handleShowHeaderAndFooter = () => {
+    if (pathname === "/signin" || pathname === "/signup") {
+      setShowHeaderAndFooter((prev) => !prev);
+    }
+  };
+
+  useEffect(() => {
+    handleShowHeaderAndFooter();
+  }, []);
   return (
-    <div className="w-full py-6 md:py-10 px-4 md:px-8 lg:px-36 flex flex-col md:flex-row items-center justify-center md:justify-between gap-4 md:gap-6 text-center">
-      <div className="flex items-center justify-center gap-4">
-        {FooterIcons.map((icon) => (
-          <Link
-            key={icon.id}
-            className="rounded-full border border-gray-200 hover:border-gray-800/90 dark:border-gray-800 dark:hover:border-gray-200 bg-gray-50 w-8 h-8 flex items-center justify-center overflow-hidden"
-            href="#"
-          >
-            {icon.icon}
-            <span className="sr-only">{icon.name}</span>
-          </Link>
-        ))}
-      </div>
-      <nav className="flex space-x-5 md:flex-row gap-1 md:gap-4">
-        {FooterLinks.map((item) => (
-          <Link
-            key={item.id}
-            className="text-xs md:text-sm font-medium text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
-            href={item.url}
-          >
-            {item.name}
-          </Link>
-        ))}
-      </nav>
-    </div>
+    <>
+      {showHeaderAndFooter && (
+        <div className="w-full py-6 md:py-10 px-4 md:px-8 lg:px-36 flex flex-col md:flex-row items-center justify-center md:justify-between gap-4 md:gap-6 text-center">
+          <div className="flex items-center justify-center gap-4">
+            {FooterIcons.map((icon) => (
+              <Link
+                key={icon.id}
+                className="rounded-full border border-gray-200 hover:border-gray-800/90 dark:border-gray-800 dark:hover:border-gray-200 bg-gray-50 w-8 h-8 flex items-center justify-center overflow-hidden"
+                href="#"
+              >
+                {icon.icon}
+                <span className="sr-only">{icon.name}</span>
+              </Link>
+            ))}
+          </div>
+          <nav className="flex space-x-5 md:flex-row gap-1 md:gap-4">
+            {FooterLinks.map((item) => (
+              <Link
+                key={item.id}
+                className="text-xs md:text-sm font-medium text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
+                href={item.url}
+              >
+                {item.name}
+              </Link>
+            ))}
+          </nav>
+        </div>
+      )}
+    </>
   );
 };
 
