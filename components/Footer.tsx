3052852aca29a256cb31149cfd70c7f80a0b0ba4
Trend20 <enockomondi305@gmail.com>
2024-03-27 20:11:19 +0300

    removing footer and header conditions

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -57,33 +57,31 @@ const Footer = () => {
   }, []);
   return (
     <>
-      {showHeaderAndFooter && (
-        <div className="w-full py-6 md:py-10 px-4 md:px-8 lg:px-36 flex flex-col md:flex-row items-center justify-center md:justify-between gap-4 md:gap-6 text-center">
-          <div className="flex items-center justify-center gap-4">
-            {FooterIcons.map((icon) => (
-              <Link
-                key={icon.id}
-                className="rounded-full border border-gray-200 hover:border-gray-800/90 dark:border-gray-800 dark:hover:border-gray-200 bg-gray-50 w-8 h-8 flex items-center justify-center overflow-hidden"
-                href="#"
-              >
-                {icon.icon}
-                <span className="sr-only">{icon.name}</span>
-              </Link>
-            ))}
-          </div>
-          <nav className="flex space-x-5 md:flex-row gap-1 md:gap-4">
-            {FooterLinks.map((item) => (
-              <Link
-                key={item.id}
-                className="text-xs md:text-sm font-medium text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
-                href={item.url}
-              >
-                {item.name}
-              </Link>
-            ))}
-          </nav>
+      <div className="w-full py-6 md:py-10 px-4 md:px-8 lg:px-36 flex flex-col md:flex-row items-center justify-center md:justify-between gap-4 md:gap-6 text-center">
+        <div className="flex items-center justify-center gap-4">
+          {FooterIcons.map((icon) => (
+            <Link
+              key={icon.id}
+              className="rounded-full border border-gray-200 hover:border-gray-800/90 dark:border-gray-800 dark:hover:border-gray-200 bg-gray-50 w-8 h-8 flex items-center justify-center overflow-hidden"
+              href="#"
+            >
+              {icon.icon}
+              <span className="sr-only">{icon.name}</span>
+            </Link>
+          ))}
         </div>
-      )}
+        <nav className="flex space-x-5 md:flex-row gap-1 md:gap-4">
+          {FooterLinks.map((item) => (
+            <Link
+              key={item.id}
+              className="text-xs md:text-sm font-medium text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
+              href={item.url}
+            >
+              {item.name}
+            </Link>
+          ))}
+        </nav>
+      </div>
     </>
   );
 };
--- components/Header.tsx
+++ components/Header.tsx
@@ -70,82 +70,78 @@ const Header = () => {
 
   const isScrolled = scrollY > 0;
   return (
-    <>
-      {showHeaderAndFooter && (
+    <div
+      className={`fixed top-0 left-0 w-full px-4 md:px-8 lg:px-36 py-5 z-50 ${
+        isScrolled ? "bg-boxdark shadow-md transition-all duration-300" : ""
+      }`}
+    >
+      <div className="w-full flex justify-between items-center">
+        <div className="flex justify-start items-center flex-shrink-0">
+          <Link className="flex items-center space-x-2" href="/">
+            <Image
+              src="/logo3.png"
+              alt="logo"
+              width="200"
+              height="200"
+              className="w-25 md:w-25"
+            />
+          </Link>
+        </div>
         <div
-          className={`fixed top-0 left-0 w-full px-4 md:px-8 lg:px-36 py-5 z-50 ${
-            isScrolled ? "bg-boxdark shadow-md transition-all duration-300" : ""
+          className={`lg:flex lg:w-3/4 lg:items-center lg:flex-row lg:border-0 lg:bg-transparent lg:relative lg:top-0 lg:justify-between lg:border-none ${
+            showNavLinks
+              ? "flex absolute border-2 px-3  flex-col border-teal bg-meta-4 w-full top-20 left-0"
+              : "hidden"
           }`}
         >
-          <div className="w-full flex justify-between items-center">
-            <div className="flex justify-start items-center flex-shrink-0">
-              <Link className="flex items-center space-x-2" href="/">
-                <Image
-                  src="/logo3.png"
-                  alt="logo"
-                  width="200"
-                  height="200"
-                  className="w-25 md:w-25"
-                />
+          <nav
+            className={`flex lg:w-1/2 lg:justify-end  lg:bg-transparent w-full rounded-md items-start md:items-center flex-col lg:flex-row space-y-4 md:space-y-0 md:space-x-6 md:border-none py-4 md:py-0`}
+          >
+            {headerData.map((item: any) => (
+              <Link
+                key={item.id}
+                className="text-normal capitalize font-medium"
+                href={item.url}
+              >
+                {item.name}
               </Link>
-            </div>
-            <div
-              className={`lg:flex lg:w-3/4 lg:items-center lg:flex-row lg:border-0 lg:bg-transparent lg:relative lg:top-0 lg:justify-between lg:border-none ${
-                showNavLinks
-                  ? "flex absolute border-2 px-3  flex-col border-teal bg-meta-4 w-full top-20 left-0"
-                  : "hidden"
-              }`}
-            >
-              <nav
-                className={`flex lg:w-1/2 lg:justify-end  lg:bg-transparent w-full rounded-md items-start md:items-center flex-col lg:flex-row space-y-4 md:space-y-0 md:space-x-6 md:border-none py-4 md:py-0`}
+            ))}
+          </nav>
+          <div className="flex lg:justify-end lg:w-1/4 py-1 items-center flex-shrink-0">
+            {!session ? (
+              <Link
+                href={"/auth/signin"}
+                className="md:flex justify-center lg:items-center font-medium w-36 bg-teal p-3 rounded-md outline-none"
               >
-                {headerData.map((item: any) => (
-                  <Link
-                    key={item.id}
-                    className="text-normal capitalize font-medium"
-                    href={item.url}
-                  >
-                    {item.name}
-                  </Link>
-                ))}
-              </nav>
-              <div className="flex lg:justify-end lg:w-1/4 py-1 items-center flex-shrink-0">
-                {!session ? (
-                  <Link
-                    href={"/auth/signin"}
-                    className="md:flex justify-center lg:items-center font-medium w-36 bg-teal p-3 rounded-md outline-none"
-                  >
-                    Try for Free
-                  </Link>
-                ) : (
-                  <button
-                    onClick={() => signOut({ redirect: true })}
-                    className="hidden md:flex items-center p-3 border rounded"
-                  >
-                    <i className="mr-3">
-                      <TbLogout size={25} />
-                    </i>
-                    Logout
-                  </button>
-                )}
-              </div>
-            </div>
-            <div className="lg:hidden">
+                Try for Free
+              </Link>
+            ) : (
               <button
-                className="block md:hidden focus:outline-none"
-                onClick={() => setShowNavLinks(!showNavLinks)}
+                onClick={() => signOut({ redirect: true })}
+                className="hidden md:flex items-center p-3 border rounded"
               >
-                {showNavLinks ? (
-                  <AiOutlineClose size={24} />
-                ) : (
-                  <AiOutlineMenu size={24} />
-                )}
+                <i className="mr-3">
+                  <TbLogout size={25} />
+                </i>
+                Logout
               </button>
-            </div>
+            )}
           </div>
         </div>
-      )}
-    </>
+        <div className="lg:hidden">
+          <button
+            className="block md:hidden focus:outline-none"
+            onClick={() => setShowNavLinks(!showNavLinks)}
+          >
+            {showNavLinks ? (
+              <AiOutlineClose size={24} />
+            ) : (
+              <AiOutlineMenu size={24} />
+            )}
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
 
