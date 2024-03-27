3e76c0d011256fc82ae601eec228c1d1711ac16e
Trend20 <enockomondi305@gmail.com>
2024-03-27 18:53:19 +0300

    hidding header and footer on login and signup routes

--- components/Header.tsx
+++ components/Header.tsx
@@ -41,6 +41,17 @@ const Header = () => {
   const [scrollY, setScrollY] = useState(0);
   // Inside your component
   const [showNavLinks, setShowNavLinks] = useState(false);
+  const [showHeaderAndFooter, setShowHeaderAndFooter] = useState(false);
+
+  const handleShowHeaderAndFooter = () => {
+    if (pathname === "/signin" || pathname === "/signup") {
+      setShowHeaderAndFooter((prev) => !prev);
+    }
+  };
+
+  useEffect(() => {
+    handleShowHeaderAndFooter();
+  }, []);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -59,78 +70,82 @@ const Header = () => {
 
   const isScrolled = scrollY > 0;
   return (
-    <div
-      className={`fixed top-0 left-0 w-full px-4 md:px-8 lg:px-36 py-5 z-50 ${
-        isScrolled ? "bg-boxdark shadow-md transition-all duration-300" : ""
-      }`}
-    >
-      <div className="w-full flex justify-between items-center">
-        <div className="flex justify-start items-center flex-shrink-0">
-          <Link className="flex items-center space-x-2" href="/">
-            <Image
-              src="/logo3.png"
-              alt="logo"
-              width="200"
-              height="200"
-              className="w-25 md:w-25"
-            />
-          </Link>
-        </div>
+    <>
+      {showHeaderAndFooter && (
         <div
-          className={`lg:flex lg:w-3/4 lg:items-center lg:flex-row lg:border-0 lg:bg-transparent lg:relative lg:top-0 lg:justify-between lg:border-none ${
-            showNavLinks
-              ? "flex absolute border-2 px-3  flex-col border-teal bg-meta-4 w-full top-20 left-0"
-              : "hidden"
+          className={`fixed top-0 left-0 w-full px-4 md:px-8 lg:px-36 py-5 z-50 ${
+            isScrolled ? "bg-boxdark shadow-md transition-all duration-300" : ""
           }`}
         >
-          <nav
-            className={`flex lg:w-1/2 lg:justify-end  lg:bg-transparent w-full rounded-md items-start md:items-center flex-col lg:flex-row space-y-4 md:space-y-0 md:space-x-6 md:border-none py-4 md:py-0`}
-          >
-            {headerData.map((item: any) => (
-              <Link
-                key={item.id}
-                className="text-normal capitalize font-medium"
-                href={item.url}
-              >
-                {item.name}
+          <div className="w-full flex justify-between items-center">
+            <div className="flex justify-start items-center flex-shrink-0">
+              <Link className="flex items-center space-x-2" href="/">
+                <Image
+                  src="/logo3.png"
+                  alt="logo"
+                  width="200"
+                  height="200"
+                  className="w-25 md:w-25"
+                />
               </Link>
-            ))}
-          </nav>
-          <div className="flex lg:justify-end lg:w-1/4 py-1 items-center flex-shrink-0">
-            {!session ? (
-              <Link
-                href={"/auth/signin"}
-                className="md:flex justify-center lg:items-center font-medium w-36 bg-teal p-3 rounded-md outline-none"
+            </div>
+            <div
+              className={`lg:flex lg:w-3/4 lg:items-center lg:flex-row lg:border-0 lg:bg-transparent lg:relative lg:top-0 lg:justify-between lg:border-none ${
+                showNavLinks
+                  ? "flex absolute border-2 px-3  flex-col border-teal bg-meta-4 w-full top-20 left-0"
+                  : "hidden"
+              }`}
+            >
+              <nav
+                className={`flex lg:w-1/2 lg:justify-end  lg:bg-transparent w-full rounded-md items-start md:items-center flex-col lg:flex-row space-y-4 md:space-y-0 md:space-x-6 md:border-none py-4 md:py-0`}
               >
-                Try for Free
-              </Link>
-            ) : (
+                {headerData.map((item: any) => (
+                  <Link
+                    key={item.id}
+                    className="text-normal capitalize font-medium"
+                    href={item.url}
+                  >
+                    {item.name}
+                  </Link>
+                ))}
+              </nav>
+              <div className="flex lg:justify-end lg:w-1/4 py-1 items-center flex-shrink-0">
+                {!session ? (
+                  <Link
+                    href={"/auth/signin"}
+                    className="md:flex justify-center lg:items-center font-medium w-36 bg-teal p-3 rounded-md outline-none"
+                  >
+                    Try for Free
+                  </Link>
+                ) : (
+                  <button
+                    onClick={() => signOut({ redirect: true })}
+                    className="hidden md:flex items-center p-3 border rounded"
+                  >
+                    <i className="mr-3">
+                      <TbLogout size={25} />
+                    </i>
+                    Logout
+                  </button>
+                )}
+              </div>
+            </div>
+            <div className="lg:hidden">
               <button
-                onClick={() => signOut({ redirect: true })}
-                className="hidden md:flex items-center p-3 border rounded"
+                className="block md:hidden focus:outline-none"
+                onClick={() => setShowNavLinks(!showNavLinks)}
               >
-                <i className="mr-3">
-                  <TbLogout size={25} />
-                </i>
-                Logout
+                {showNavLinks ? (
+                  <AiOutlineClose size={24} />
+                ) : (
+                  <AiOutlineMenu size={24} />
+                )}
               </button>
-            )}
+            </div>
           </div>
         </div>
-        <div className="lg:hidden">
-          <button
-            className="block md:hidden focus:outline-none"
-            onClick={() => setShowNavLinks(!showNavLinks)}
-          >
-            {showNavLinks ? (
-              <AiOutlineClose size={24} />
-            ) : (
-              <AiOutlineMenu size={24} />
-            )}
-          </button>
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
