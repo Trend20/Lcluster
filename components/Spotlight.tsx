27279b5c93dc04d06422b99e75a1bc811cf84ccd
Trend20 <enockomondi305@gmail.com>
2024-04-09 20:56:54 +0300

    adding brands to the website

--- components/Spotlight.tsx
+++ components/Spotlight.tsx
@@ -1,22 +1,31 @@
+import { brands } from "@/data/brands";
+import Image from "next/image";
 import React from "react";
-import Link from "next/link";
 
 const Spotlight = () => {
   return (
-    <section className="w-full py-8 lg:py-5 px-4 md:px-6 xl:px-36 lg:mt-10 bg-boxdark lg:w-1/2 rounded-br-lg">
+    <section className="w-full py-8 lg:py-5 px-4 md:px-6 xl:px-36 lg:mt-10 lg:w-1/2 rounded-br-lg">
       <div className="flex flex-col items-center">
         <div className="text-center">
-          <p className="max-w-full lg:max-w-[700px] text-xl font-bold lg:text-normal text-gray-500 dark:text-gray-400">
-            The platform for rapid progress. Made by Developers, for Developers!
+          <p className="max-w-full lg:max-w-[700px] text-lg lg:text-normal text-gray-500 dark:text-gray-400">
+            Trusted by developers from:
           </p>
         </div>
         <div className="flex items-center justify-center w-full mt-8">
-          <Link
-            className="inline-flex items-center w-full lg:w-40 justify-center rounded-md border-2 font-bold border-teal px-4 py-3 text-sm md:text-base shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
-            href="/libraries/javascript"
-          >
-            Try Now
-          </Link>
+          {brands.map((item) => (
+            <div
+              key={item.id}
+              className="mx-auto flex w-full items-center justify-center"
+            >
+              <Image
+                alt={item.name}
+                className="aspect-[2/1] cursor-pointer overflow-hidden rounded-lg object-contain object-center"
+                height="100"
+                src={item.icon}
+                width="100"
+              />
+            </div>
+          ))}
         </div>
       </div>
     </section>
---
+++ data/brands.ts
@@ -0,0 +1,32 @@
+export const brands = [
+  {
+    id: 1,
+    icon: "/icons/google.svg",
+    name:'google'
+  },
+  {
+    id: 2,
+    icon: "/icons/hashnode.svg",
+    name:'hashnode'
+  },
+  {
+    id: 3,
+    icon: "/icons/microsoft.svg",
+    name:'microsoft'
+  },
+  {
+    id: 4,
+    icon: "/icons/instagram.svg",
+    name:'instagram'
+  },
+  {
+    id: 5,
+    icon: "/icons/google.svg",
+    name:'google'
+  },
+  {
+    id: 6,
+    icon: "/icons/hashnode.svg",
+    name:'hashnode'
+  },
+];
