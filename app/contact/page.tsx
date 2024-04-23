778241a91fd6203f89402d3edff6083db28d6493
Trend20 <enockomondi305@gmail.com>
2024-04-23 18:41:56 +0300

    contact form ui fix

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,12 +3,12 @@ import NewsLatter from "@/app/contact/components/NewsLetter";
 
 const Contact = () => {
     return (
-        <section id="contact" className="overflow-hidden py-16 md:py-20 lg:py-28">
+        <section id="contact" className="overflow-hidden px-50 mt-40 md:py-20 lg:py-30">
             <div className="container">
                 <div className="-mx-4 flex flex-wrap">
                     <div className="w-full px-4 lg:w-7/12 xl:w-8/12">
                         <div
-                            className="mb-12 rounded-sm bg-white px-8 py-11 shadow-three dark:bg-gray-dark sm:p-[55px] lg:mb-5 lg:px-8 xl:p-[55px]"
+                            className="mb-12 rounded-sm px-8 py-11 shadow-three dark:bg-gray-dark sm:p-[55px] lg:mb-5 lg:px-8 xl:p-[55px]"
                             data-wow-delay=".15s
               "
                         >
@@ -31,7 +31,7 @@ const Contact = () => {
                                             <input
                                                 type="text"
                                                 placeholder="Enter your name"
-                                                className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
+                                                className="w-full rounded-sm border bg-transparent px-6 py-3 text-base text-body-color outline-none focus:border-teal dark:border-transparent dark:text-body-color-dark dark:shadow-two dark:focus:border-teal dark:focus:shadow-none"
                                             />
                                         </div>
                                     </div>
@@ -46,7 +46,7 @@ const Contact = () => {
                                             <input
                                                 type="email"
                                                 placeholder="Enter your email"
-                                                className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
+                                                className="w-full rounded-sm border bg-transparent px-6 py-3 text-base text-body-color outline-none focus:border-teal dark:border-transparent dark:text-body-color-dark dark:shadow-two dark:focus:border-teal dark:focus:shadow-none"
                                             />
                                         </div>
                                     </div>
@@ -62,13 +62,13 @@ const Contact = () => {
                                                 name="message"
                                                 rows={5}
                                                 placeholder="Enter your Message"
-                                                className="border-stroke w-full resize-none rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
+                                                className="w-full resize-none rounded-sm border bg-transparent px-6 py-3 text-base text-body-color outline-none focus:border-teal dark:border-transparent dark:bg-transparent dark:text-body-color-dark dark:shadow-two dark:focus:border-teal dark:focus:shadow-none"
                                             ></textarea>
                                         </div>
                                     </div>
                                     <div className="w-full px-4">
                                         <button
-                                            className="rounded-sm bg-primary px-9 py-4 text-base font-medium text-white shadow-submit duration-300 hover:bg-primary/90 dark:shadow-submit-dark">
+                                            className="rounded-sm bg-teal px-9 py-4 text-base font-medium shadow-submit duration-300 dark:shadow-submit-dark">
                                             Submit Ticket
                                         </button>
                                     </div>
